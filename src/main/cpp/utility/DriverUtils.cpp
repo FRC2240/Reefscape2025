6e0d7c0743a89607b1866e05178e4c5b9addcbf1
cynosure <cynosure@disroot.org>
2025-01-09 18:59:15 -0700

    minor change

--- src/main/cpp/utility/DriverUtils.cpp
+++ src/main/cpp/utility/DriverUtils.cpp
@@ -5,9 +5,9 @@ void DriverUtils::DriverTeleopPeriodic() {
   frc2::CommandXboxController m_stick{0};
 
   double time = frc::DriverStation::GetMatchTime().value();
-  // double result = units::unit_cast<double>(time);
+ 
   frc::SmartDashboard::PutNumber("time", time);
-  if (time <= 45 && time > 45 - ALERT_RUMBLE_DURATION) {
+  if (time <= 45.0 && time > 45.0 - ALERT_RUMBLE_DURATION) {
     m_stick.SetRumble(frc::GenericHID::kBothRumble, 1);
   } else {
     m_stick.SetRumble(frc::GenericHID::kBothRumble, 0);
