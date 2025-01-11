0ff18591baf471ab01d26938b419a932cd6be405
Tiernan2026 <144292343+Tiernan2026@users.noreply.github.com>
2025-01-11 16:21:36 -0700

    intake & extake

--- src/main/cpp/subsystems/Grabber.cpp
+++ src/main/cpp/subsystems/Grabber.cpp
@@ -6,5 +6,29 @@ Grabber::Grabber()
     MotorUtils::Motor::LogValues log_values{true, true, true, true, true};
     MotorUtils::Motor right_grabber_motor{&m_right_motor, CONSTANTS::GRABBER::PID, log_values};
     MotorUtils::Motor left_grabber_motor{&m_left_motor, CONSTANTS::GRABBER::PID, log_values};
-    // CONSTANTS::GRABBER::PidValue; unsure
+
+    AddPID(right_grabber_motor);
+    AddPID(left_grabber_motor);
+    SetPID();
+};
+
+void Grabber::spin(units::turns_per_second_t speed){
+    ctre::phoenix6::controls::VelocityVoltage velocity{speed};
+    m_right_motor.SetControl(velocity);
+    m_left_motor.SetControl(velocity);
+};
+
+void Grabber::intake(units::turns_per_second_t speed){
+    spin(CONSTANTS::GRABBER::INTAKE_VELOCITY);
+    if(units::millimeter_t{Grabber_sensor.GetRange()} < CONSTANTS::GRABBER::DEFAULT_DIST_TOF){
+            spin(0_tps);
+    };
+};
+
+frc2::CommandPtr Grabber::extake(units::turns_per_second_t speed, units::second_t time){
+    return frc2::cmd::Run(
+        [this, speed] {
+            spin(speed);
+        }, {this}
+    ).WithTimeout(time);
 };
