01e3666e5aa4e82e77fbe11ad5ff00ec1963e178
Tiernan2026 <144292343+Tiernan2026@users.noreply.github.com>
2025-01-11 11:00:34 -0700

    fixing issues rebase

--- src/main/include/subsystems/Grabber.h
+++ src/main/include/subsystems/Grabber.h
@@ -1,11 +1,12 @@
 #include <ctre/phoenix6/TalonFX.hpp>
 #include "Constants.h"
 #include <frc2/command/SubsystemBase.h>
+#include <utility/MotorUtils.h>
 
 class Grabber : public frc2::SubsystemBase {
     public:
         Grabber();
     private:
-        ctre::phoenix6::hardware::TalonFX right_grabber_motor{CONSTANTS::GRABBER::right_grabber_motor_CAN_ID };
-        ctre::phoenix6::hardware::TalonFX left_grabber_motor{CONSTANTS::GRABBER::left_grabber_motor_CAN_ID};
+        ctre::phoenix6::hardware::TalonFX m_right_motor{CONSTANTS::GRABBER::right_grabber_motor_CAN_ID };
+        ctre::phoenix6::hardware::TalonFX m_left_motor{CONSTANTS::GRABBER::left_grabber_motor_CAN_ID};
 };
