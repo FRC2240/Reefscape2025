879567cf821d226b94ac2314105458d76332db44
Tiernan2026 <144292343+Tiernan2026@users.noreply.github.com>
2025-01-11 16:21:36 -0700

    intake & extake

--- src/main/include/subsystems/Grabber.h
+++ src/main/include/subsystems/Grabber.h
@@ -2,13 +2,22 @@
 #include "Constants.h"
 #include <frc2/command/SubsystemBase.h>
 #include <utility/MotorUtils.h>
+#include "utility/BetterSubsystemBase.h"
+#include <TimeOfFlight.h>
+#include <frc2/command/Commands.h>
 
-class Grabber : public frc2::SubsystemBase
+
+class Grabber : public BetterSubsystemBase
 {
 public:
     Grabber();
 
 private:
-    ctre::phoenix6::hardware::TalonFX m_right_motor{CONSTANTS::GRABBER::RIGHT_CAN_ID};
-    ctre::phoenix6::hardware::TalonFX m_left_motor{CONSTANTS::GRABBER::LEFT_CAN_ID};
+    ctre::phoenix6::hardware::TalonFX m_right_motor{CONSTANTS::GRABBER::RIGHT_ID};
+    ctre::phoenix6::hardware::TalonFX m_left_motor{CONSTANTS::GRABBER::LEFT_ID};
+    pwf::TimeOfFlight Grabber_sensor{CONSTANTS::GRABBER::TOF_ID};
+
+    void spin(units::turns_per_second_t speed);
+    void intake(units::turns_per_second_t speed);
+    frc2::CommandPtr extake(units::turns_per_second_t speed, units::second_t time);
 };
