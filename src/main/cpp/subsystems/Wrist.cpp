6ccfaa5032edb02c2773ebb0dd927e40112cb956
LaurensDSchunk <laurensdschunk@gmail.com>
2025-01-16 08:39:13 -0700

    Changed wrist controls

--- src/main/cpp/subsystems/Wrist.cpp
+++ src/main/cpp/subsystems/Wrist.cpp
@@ -1,6 +1,6 @@
 #include "subsystems/Wrist.h"
 
-#include "frc2/command/FunctionalCommand.h"
+#include "frc2/command/RunCommand.h"
 
 Wrist::Wrist() {
     MotorUtils::Motor::LogValues logValues {true, true, true};
@@ -12,9 +12,11 @@ Wrist::Wrist() {
 }
 
 void Wrist::set_angle(units::angle::degree_t angle) {
-    // Not sure if this is how I'm supposed to do it but it can change
-    ctre::phoenix6::controls::PositionVoltage req{angle};
-    m_motor.SetControl(req);
+    m_motor.SetControl(ctre::phoenix6::controls::PositionVoltage {angle});
+}
+
+void Wrist::set_angle(units::angle::turn_t angle) {
+    m_motor.SetControl(ctre::phoenix6::controls::PositionVoltage {angle});
 }
 
 units::degree_t Wrist::get_angle()
@@ -22,34 +24,9 @@ units::degree_t Wrist::get_angle()
   return m_motor.GetPosition().GetValue();
 }
 
-frc2::CommandPtr Wrist::set_angle_cmd(units::degree_t angle) {
-    std::function<void()> init = [this] {};
-    std::function<void()> periodic = [this, &angle]()
-    {
-        try
-        {
-            set_angle(angle);
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-    };
-
-    std::function<bool()> is_finished = [this, &angle]() -> bool 
-    {
-        try
-        {
-            return CONSTANTS::IN_THRESHOLD<units::turn_t>(get_angle(), angle, 1_deg);
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-        return false;
-    };
-
-    std::function<void(bool IsInterrupted)> end = [this](bool IsInterrupted) {};
-
-    return frc2::FunctionalCommand(init, periodic, end, is_finished, {this}).WithName("wrist_angle");
+frc2::CommandPtr Wrist::set_angle_command(units::degree_t pos) {
+    return frc2::RunCommand([this, pos] {
+        set_angle(pos);
+    },
+    {this}).WithName("Set Wrist Angle");
 }
