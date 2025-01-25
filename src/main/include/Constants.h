e0732881f76066ee1a11948f1de8177ed061680c
cynosure <cynosure@disroot.org>
2025-01-24 22:30:44 -0700

    I belived I fixed the issues

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -63,9 +63,10 @@ namespace CONSTANTS
             units::turn_t wrist_pos;
 
             // I don't know why this needs to be overriden but it does.  ¯\_(-_-)_/¯
-            bool operator==(TargetProfile &other)
+            bool operator==(TargetProfile other)
             {
-                return ((IN_THRESHOLD<TargetProfile>(this->elevtor_pos, other.elevtor_pos, 0.5_tr) && (IN_THRESHOLD<TargetProfile>(this->wrist_pos, other.wrist_pos, 0.5_tr)));
+                return (
+                    IN_THRESHOLD<units::turn_t>(this->elevtor_pos, other.elevtor_pos, 0.5_tr) && IN_THRESHOLD<units::turn_t>(this->wrist_pos, other.wrist_pos, 0.5_tr));
             }
         };
         constexpr TargetProfile L1{0_tr, 0_tr};
