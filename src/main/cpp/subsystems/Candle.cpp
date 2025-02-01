756ee0b34528d738050c10c1e547b04523acd394
0wenWasTaken <locust5280o@gmail.com>
2025-02-01 11:58:43 -0700

    Brown Update Candle.cpp
    Brown was a declared state and could be switched to but didn't have intended functionality.

--- src/main/cpp/subsystems/Candle.cpp
+++ src/main/cpp/subsystems/Candle.cpp
@@ -64,6 +64,7 @@ void Candle::Periodic()
     {
         state = ENABLED;
     }
+    // The BROWN state is declared(.h) and can be switched to(here), but doesn't have any functionality in the switch case, defaulting sets rgb to 255 
     else if (frc::DriverStation::GetBatteryVoltage() < 9.0) // Turn LEDs off to conserve voltage under brown
     {
         state = BROWN;
@@ -132,6 +133,12 @@ void Candle::Periodic()
 
     case ERROR:
         // Fallthrough intented
+
+    //Added fuctionality to turn off LEDs on brown
+    case BROWN:
+        m_candle.SetLEDs(0,0,0);
+        //Redirect to other state or break
+        break;
 
     default:
         m_candle.SetLEDs(255, 255, 255);
