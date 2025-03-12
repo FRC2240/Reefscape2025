f5ce97e1c33acce748423202fa1e85ee0a7a9b45
Tiernan2026 <144292343+Tiernan2026@users.noreply.github.com>
2025-03-12 16:05:38 -0600

    Motorized grabber

--- src/main/include/subsystems/Grabber.h
+++ src/main/include/subsystems/Grabber.h
@@ -15,19 +15,21 @@ public:
     frc2::CommandPtr intake(units::turns_per_second_t speed);
     frc2::CommandPtr extake();
     frc2::CommandPtr idle();
-    /* NO MOTORS, NO FUNCTIONS
-
+    
     void InitSendable(wpi::SendableBuilder &builder) override;
     void SetPID();
-    */
-
+    
     bool has_gp();
 
 private:
-    //ctre::phoenix6::hardware::TalonFX m_right_motor{CONSTANTS::GRABBER::RIGHT_ID};
-    //ctre::phoenix6::hardware::TalonFX m_left_motor{CONSTANTS::GRABBER::LEFT_ID};
+    ctre::phoenix6::hardware::TalonFX m_right_motor{CONSTANTS::GRABBER::RIGHT_ID};
+    ctre::phoenix6::hardware::TalonFX m_left_motor{CONSTANTS::GRABBER::LEFT_ID};
+
     pwf::TimeOfFlight Grabber_sensor{CONSTANTS::GRABBER::TOF_ID};
+
+    CONSTANTS::PidCoeff coeff{CONSTANTS::GRABBER::PID};
+
     //ctre::phoenix6::hardware::CANrange m_can_range{CONSTANTS::GRABBER::TOF_ID};
 
-    //void spin(units::turns_per_second_t speed);
+    void spin(units::turns_per_second_t speed);
 };
