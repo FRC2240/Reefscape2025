5e7dd43599695bb9bdfeb0abf6e063055ac15e44
cynosure <cynosure@disroot.org>
2025-03-14 17:53:18 -0600

    magic'd the motion

--- src/main/cpp/subsystems/Climber.cpp
+++ src/main/cpp/subsystems/Climber.cpp
@@ -3,6 +3,11 @@
 
 Climber::Climber()
 {
+    ctre::phoenix6::configs::TalonFXConfiguration conf{};
+    conf.MotionMagic.MotionMagicAcceleration = 250_tr_per_s_sq;
+    conf.MotionMagic.MotionMagicCruiseVelocity = 35_tps;
+    conf.Slot0.GravityType = ctre::phoenix6::signals::GravityTypeValue::Arm_Cosine;
+    m_motor.GetConfigurator().Apply(conf);
     SetPID();
     m_motor.SetPosition(CONSTANTS::CLIMBER::DEFAULT_POS);
 }
@@ -24,7 +29,9 @@ frc2::CommandPtr Climber::set_position_command(units::angle::turn_t pos)
     return frc2::RunCommand([this, pos]
                             { set_position(pos); },
                             {this})
-        .ToPtr();
+        .Until([this, pos]
+
+               { return CONSTANTS::IN_THRESHOLD<units::angle::turn_t>(get_angle(), pos, CONSTANTS::CLIMBER::POSITION_THRESHOLD); });
 }
 
 frc2::CommandPtr Climber::idle_command()
@@ -44,7 +51,7 @@ frc2::CommandPtr Climber::climb_command()
 
 void Climber::set_position(units::angle::turn_t pos)
 {
-    m_motor.SetControl(ctre::phoenix6::controls::PositionTorqueCurrentFOC{pos});
+    m_motor.SetControl(control_req.WithPosition(pos));
 };
 
 units::degree_t Climber::get_angle()
