93721cb265eb54d055e688f12e21d9ab26941c2d
TavorPoleg <Tavor.Poleg.MTV@scienceandtech.org>
2025-04-04 16:02:28 -0700

    hi

---
+++ src/main/cpp/subsystems/FunnelPowered.cpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "subsystems/FunnelPoweredVroomVroom.h"
+#include "frc/smartdashboard/SmartDashboard.h"
+#include "frc2/command/Commands.h"
+
+
+frc2::CommandPtr PoweredFun::spin(units::ampere_t current)
+{
+    ctre::phoenix6::controls::TorqueCurrentFOC req{current};
+    m_motor.SetControl(req);
+};
