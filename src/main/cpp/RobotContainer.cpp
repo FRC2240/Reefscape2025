73b2de4e43db99836cdefb30da81268ff0785f36
cynosure <cynosure@disroot.org>
2025-04-12 18:29:31 -0600

    Au revoir, 2240. So long and thanks for all the fish
    The barge auto works, the 3gp auto very much doesn't work. The elevator
    is currently too slow for unkown reasons. Time for champs.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -165,7 +165,8 @@ frc2::CommandPtr RobotContainer::set_state(CONSTANTS::MANIPULATOR_STATES::Manipu
   {
     return m_wrist.set_angle_command(target.wrist_pos).AndThen(m_elevator.set_position_command(target.elevtor_pos));
   }
-  if (target == CONSTANTS::MANIPULATOR_STATES::L4 && last_state == CONSTANTS::MANIPULATOR_STATES::IDLE && !frc::DriverStation::IsAutonomous())
+  // if (target == CONSTANTS::MANIPULATOR_STATES::L4 && last_state == CONSTANTS::MANIPULATOR_STATES::IDLE && !frc::DriverStation::IsAutonomous())
+  if (target == CONSTANTS::MANIPULATOR_STATES::L4)
   {
     return m_elevator.set_position_command(target.elevtor_pos).AndThen(m_wrist.set_angle_command(target.wrist_pos));
   }
